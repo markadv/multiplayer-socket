40b19ded388fc19a27480c381cefa2e08327ac6f
Mark Timothy Advento <mark@adventodesign.com>
2022-09-07 00:04:01 +0800

    Added status/chat system

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 1337;
 const server = app.listen(process.env.PORT || port);
 app.use(express.static(__dirname + "/public"));
+// const io = require("socket.io")(server, { upgrade: false, transports: ["websocket", "polling"] });
 const io = require("socket.io")(server);
 let users = {},
     coins = { "7x7": { x: 7, y: 7 } },
@@ -16,6 +17,7 @@ io.on("connection", function (socket) {
     /* Initialize spawn and disconnect */
     socket.on("userSpawn", function (data) {
         users[socket.id] = data;
+        console.log(users);
         socket.emit("playerInitial", users);
         socket.emit("coinsUpdate", coins);
         socket.emit("leaderboardUpdate", leaderboard);
@@ -25,35 +27,38 @@ io.on("connection", function (socket) {
         //remove player from client and database
         delete users[socket.id];
         socket.broadcast.emit("removePlayer", socket.id);
-        socket.broadcast.emit("playerUpdate", users);
     });
-    /* Player actions (Must be fully optimized) */
+    /* Player actions (Must be fully optimized)
+    Update dependent. */
     socket.on("playerMove", function (data) {
         users[data.id] = data;
-        io.emit("playerUpdate", users);
     });
+    /* Change Name/Color Update dependent. */
     socket.on("profileUpdate", function (data) {
-        users[data.id][data.profile] = data.value;
-        io.emit("playerUpdate", users);
+        users[socket.id][data.profile] = data.value;
+        // io.emit("playerUpdate", users);
     });
     socket.on("coinsAdd", function (data) {
-        users[data.id]["coins"]++;
-        if (users[data.id]["coins"] >= 15) {
-            users[data.id]["coins"] = 0;
+        users[socket.id]["coins"]++;
+        if (users[socket.id]["coins"] >= 15) {
+            users[socket.id]["coins"] = 0;
             for (let key in users) {
                 users[key].coins = 0;
             }
-            leaderboard[users[data.id].name]
-                ? leaderboard[users[data.id].name]++
-                : (leaderboard[users[data.id].name] = 1);
+            leaderboard[users[socket.id].name]
+                ? leaderboard[users[socket.id].name]++
+                : (leaderboard[users[socket.id].name] = 1);
 
-            io.emit("playerWin", leaderboard);
+            io.emit("playerWin");
+            io.emit("leaderboardUpdate", leaderboard);
         }
         delete coins[data.key];
         coinSpawner();
         io.emit("coinsUpdate", coins);
     });
 });
+/* This sends constant update instead of firing based on input */
+let playerUpdateLoop = setInterval(() => io.emit("playerUpdate", users), 150);
 function coinSpawner() {
     while (Object.keys(coins).length < 4) {
         let newCoin = getRandomSafeSpot(),
@@ -66,7 +71,6 @@ function getKeyString(x, y) {
     return `${x}x${y}`;
 }
 function getRandomSafeSpot() {
-    //We don't look things up by key here, so just return an x/y
     return randomFromArray([
         { x: 1, y: 4 },
         { x: 2, y: 4 },
